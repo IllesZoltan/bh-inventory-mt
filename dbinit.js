cf3b488353d2cac1ea1d0f0561dbebb793fc0ee0
Zoltan Illes <zoltan3illes@gmail.com>
2020-02-24 01:06:13 +0100

    update0224

--- dbinit.js
+++ dbinit.js
@@ -1,15 +1,27 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('inventory.db')
- 
-function database(namepm, categorypm){
-    db.serialize(function() {
-        db.run("CREATE TABLE products ( name VARCHAR(100), category VARCHAR(60))")
-     
-        db.prepare('INSERT INTO products VALUES (?, ?)')
-            .run(`${namepm}`, `${categorypm}`)
-    
-    
-    });
+
+function database(namepm, categorypm) {
+    if (namepm && categorypm) {
+        db.serialize(function () {
+            db.run("CREATE TABLE IF NOT EXISTS products ( name VARCHAR(100), category VARCHAR(60))")
+
+            db.prepare('INSERT INTO products VALUES (?, ?)')
+                .run(`${namepm}`, `${categorypm}`)
+        });
+    }
 }
 
-database("vasalo", "haztartas")
+function stockdb(id, namepm, count) {
+    if (id && namepm && count) {
+        db.serialize(function () {
+            db.run("CREATE TABLE IF NOT EXISTS stocks ( id VARCHAR (60), name VARCHAR(100), count VARCHAR(60))")
+
+            db.prepare('INSERT INTO stocks VALUES (?, ?, ?)')
+                .run(`${id}`, `${namepm}`, `${count}`)
+        });
+    }
+}
+
+//database("mosogep", "haztartas")
+//stockdb("1","vasalo","12")
--- inventorysrv.js
+++ inventorysrv.js
@@ -3,12 +3,16 @@ const path = require('path');
 const hbs = require('express-handlebars');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('inventory.db')
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+const url = 'http://localhost:3000'
+
 app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
 
 const items = [];
+const stockArr = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,18 +20,97 @@ app.use(express.static(path.join(__dirname, 'public')));
 	res.render('home', { items});
 });*/
 
-app.get('/', (req, res) => {
-    db.serialize(function() {
-        db.all("SELECT rowid, name, category from products", function(err, results) {
+app.get('/home', (req, res) => {
+    items.length = 0;
+
+    db.serialize(function () {
+        db.all("SELECT rowid, name, category from products", function (err, results) {
             if (err != null) {
                 res.send("Missing from database")
             }
-			items.push(results)
-			console.log(results)
-          res.render('home', {items:results})
-            
+            results.forEach(elem => {
+                items.push(elem)
+            })
+            res.render('home', {items})
         });
-      });
+    });
+})
+
+
+app.post('/newprod/:product', (req, res) => {
+    const nProd = req.params.product
+    const prodArr = nProd.split(',')
+
+    console.log(prodArr);
+
+})
+
+// app.get('/inventory', (req,res) => {
+
+//     db.serialize(function() {
+//         db.all("SELECT rowid, name from products", function(err, results) {
+//             if (err != null) {
+//                 res.send("Missing from database")
+//             }
+// 			items.push(results)
+//             res.render('inventory', {items:results})
+//         });
+//       });
+// })
+
+app.get('/stocks', (req, res) => {
+    items.length = 0;
+    stockArr.length = 0;
+
+    db.serialize(function () {
+        db.all("SELECT rowid, name, count from stocks", function (err, results) {
+            if (err != null) {
+                res.send("Missing from database")
+            }
+            results.forEach(elem => {
+                stockArr.push(elem)
+            })
+        });
+        
+        db.all("SELECT rowid, name FROM products", (err, res) => {
+            if (err) res.send("Missing from database")
+            res.forEach(elem => {
+                items.push(elem)
+            })
+            items.forEach(elem => {
+                stockArr.forEach(sel => {
+                    if(elem.rowid !== sel.rowid){
+                        stockArr.push(elem)
+                    }
+                })
+            })
+        })
+        res.render('inventory', {stockArr})
+    });
+})
+
+app.put('/stock_update/:data', (req,res) => {
+    const data = req.params.data
+    const dataArr = data.split(',')
+    const newID = dataArr[0]
+    const newCnt = dataArr[2]
+
+    stockArr.forEach(s_elem => {    
+        if(s_elem.count){
+            db.serialize(() => {
+                db.all(`UPDATE stocks SET count = ${newCnt} WHERE id = ${newID}`, (err,result) => {
+                    if(err) {result.send(`Error aqured ${err}`);}
+
+                    res.redirect('/stocks')
+
+                    console.log('mennyiség: ',s_elem.count);
+                })
+            })
+        }
+    })
+})
+
+app.post('/stock_newnr:data', (req, res) => {
 
 })
 
